a4e4b7b5534285247b37719483ae43f3893f8efd
BUDHA GANESH <budhaganesh73@gmail.com>
2026-01-08 16:56:37 +0530

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,12 @@
+#simple caluclater operatipons
+#variiable and operators
+a=10
+b=20
+addition=a+b
+print("sum of two numbers is:",addition)
+print("subtraction of two numbers is:",a-b)
+print("multiplication of two numbers is:",a*b)
+print("division of two numbers is:",b/a)
+print("floor division of two numbers:",b//a)
+print("modulo division of two numbers:",b%a)
+print("power of two numbers:",b**a)
