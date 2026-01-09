7802fae850be2fce7f64b4779786511e19d27a8c
BUDHA GANESH <budhaganesh73@gmail.com>
2026-01-09 22:41:42 +0530

    Add files via upload

---
+++ day4 if_eles statment.py
@@ -0,0 +1,9 @@
+#IF ELSE STATEMENT
+
+#Ex-1:Check wheather given number is zero or not
+num = int(input())
+if num ==0:
+    print("Entered number is zero.")
+else:
+    print("Entered value  is not zero.")
+    
