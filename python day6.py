534218869721c4716d2d7ed7499a81c1b897a2a4
BUDHA GANESH <budhaganesh73@gmail.com>
2026-01-12 20:31:34 +0530

    Add files via upload

---
+++ python day6.py
@@ -0,0 +1,11 @@
+n=int(input("enter the number"))
+if n%2==0:
+    if n>-1:
+        print("number is +ve even")
+    else:
+            print("number is -ve even")
+else:
+    if n>-1:
+        print("number is +ve odd")
+    else:
+        print("number is -ve odd")
