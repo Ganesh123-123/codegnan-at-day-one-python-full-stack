2cafcb12d4c41df07c3d2c226552b6440f2510ad
BUDHA GANESH <budhaganesh73@gmail.com>
2026-01-12 20:30:50 +0530

    Add files via upload

---
+++ DAY6 PYTHON.PY
@@ -0,0 +1,9 @@
+#check wich one is even or odd number in the given list of numbers
+"""lst=[2,4,3,5,6,7,9]
+
+for num in 1st:
+    if num%2==0:
+        print(num,"is even")
+        else:
+            print(num,"is odd")
+print("program done")"""
